3910f284e060a5f218e733f5d0737285b2a129a5
Jan Wolski <jan@wolski.fi>
2016-01-08 19:16:08 +0200

    added deserialization

--- serialize.js
+++ serialize.js
@@ -34,7 +34,30 @@ window.Serializer = (function(){
         return "[" + output.join(",") + "]";
 	}
     
+    function deserialize(str){
+        var list = JSON.parse(str),
+            obj,
+            index;
+        if( !Array.isArray(list) ) throw new SyntaxError("Outermost element must be Array");
+        if( list.length === 0) throw new SyntaxError("No objects to parse");
+        
+        for(var i = 0; i < list.length; i++){
+            obj = list[i];
+            for(var prop in obj){
+                if(obj.hasOwnProperty(prop) && Array.isArray(obj[prop])){
+                    index = obj[prop][0];
+                    if(typeof index !== "number" || index % 1 !== 0) throw new SyntaxError("Invalid object reference");
+                    if(index >= list.length) throw new SyntaxError("Object reference out of bounds");
+                    obj[prop] = list[index];
+                }
+            }
+		}
+        
+        return list[0];
+    }
+    
     return {
-        stringify: serialize
+        stringify: serialize,
+        parse: deserialize
     };
 })();
